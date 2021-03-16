2caac827ca804ba6e4ffe783ea46fc35d9402a30
ajaristo <ajaristo@ucalgary.ca>
2021-03-15 18:49:52 -0600

    Added scanProductTest class

---
+++ SelfCheckoutSystem - Software - Test/src/controlSoftware/ScanProductTest.java
@@ -0,0 +1,14 @@
+package controlSoftware;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ScanProductTest {
+
+	@Test
+	public void testScanProduct() {
+		fail("Not yet implemented");
+	}
+
+}
