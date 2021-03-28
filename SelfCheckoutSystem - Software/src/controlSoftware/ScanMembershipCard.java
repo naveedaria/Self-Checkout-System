3a9fe115078c53c396f453f373b1643124eaa0aa
PS1718 <sahotap1@hotmail.com>
2021-03-28 17:23:36 -0600

    added comments to membership class

--- SelfCheckoutSystem - Software/src/controlSoftware/ScanMembershipCard.java
+++ SelfCheckoutSystem - Software/src/controlSoftware/ScanMembershipCard.java
@@ -15,22 +15,37 @@ public class ScanMembershipCard {
 	private CardReader cardReader;
 	private Card inputCard;
 	
+	/**
+	 * Constructor.
+	 * @param selfCheckout 
+	 * 		  The specific checkout station instance.
+	 */
 	public void ScanMembershipCard(SelfCheckoutStation selfCheckout) {
 		CardReaderListenerStub cardReaderListener = new CardReaderListenerStub();
 		selfCheckout.cardReader.register(cardReaderListener);
 		this.cardReader = selfCheckout.cardReader;
 	}
 	
-	
-	//set the attributes of the user's card
-	public void detectCard(String type, String number, String cardholder) {
+	/**
+	 * Method to create the Card object with certain membership card attributes.
+	 * @param number 
+	 * 		  The card identification number.
+	 * @param cardHolder 
+	 * 		  The name of the card holder. 
+	 */
+	public void detectCard(String number, String cardHolder) {
 		try {
-			this.inputCard = new Card(type, number, cardholder, null, null, true, false);
+			this.inputCard = new Card("Membership", number, cardHolder, null, null, true, false);
 		}catch(SimulationException e) {
 			throw e;
 		}
 	}
 	
+	/**
+	 * Method to tap membership card and note the number before making a payment - if applicable.
+	 * @return 
+	 * 		  String - card number. 
+	 */
 	public String tapMembershipCard() throws IOException {
 		try {
 			CardData data= this.cardReader.tap(this.inputCard);
