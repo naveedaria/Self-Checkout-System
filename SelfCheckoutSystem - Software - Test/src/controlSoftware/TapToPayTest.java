b92b8edcd1025219e3cc4a9f9a402f8b417c65e9
PS1718 <sahotap1@hotmail.com>
2021-03-28 17:26:32 -0600

    starting test class for tap-to-pay process and branching flow

---
+++ SelfCheckoutSystem - Software - Test/src/controlSoftware/TapToPayTest.java
@@ -0,0 +1,24 @@
+package controlSoftware;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TapToPayTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testTapToPay() {
+		fail("Not yet implemented");
+	}
+
+}
