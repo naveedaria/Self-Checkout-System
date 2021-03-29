866505cd348ea29e4a747fcc92566e2c7c4fbb1e
PS1718 <sahotap1@hotmail.com>
2021-03-28 19:00:16 -0600

    Created subsequent test cases for invalid/valid cards with correct insertion and card limit.

--- SelfCheckoutSystem - Software - Test/src/controlSoftware/TapToPayTest.java
+++ SelfCheckoutSystem - Software - Test/src/controlSoftware/TapToPayTest.java
@@ -10,6 +10,7 @@ import java.util.Currency;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.lsmr.selfcheckout.BlockedCardException;
 import org.lsmr.selfcheckout.ChipFailureException;
 import org.lsmr.selfcheckout.devices.SimulationException;
 
@@ -29,8 +30,8 @@ public class TapToPayTest {
 		PaymentByCard cardPayment = new PaymentByCard(controlSoft.selfCheckout, "RBC Visa");
 		this.cardPayment = cardPayment;
 		
-		//Suppose the shopping balance is $25.00
-		this.totalBalance = new BigDecimal(25);
+		//Suppose the shopping balance is $100.00
+		this.totalBalance = new BigDecimal(100);
 	}
 
 	@Test
@@ -55,7 +56,7 @@ public class TapToPayTest {
 	
 	
 	@Test
-	public void testTapToPayTapDisabled() throws ChipFailureException, IOException {
+	public void testTapDisabled() throws ChipFailureException, IOException {
 		String type = "Credit Card";
 		String number = "24689";
 		String cardholder = "Bob";
@@ -74,14 +75,121 @@ public class TapToPayTest {
 			fail("Exception not expected"); 
 		}
 		boolean expected = false;
-		boolean successfulPayment = this.cardPayment.tapToPay(this.totalBalance);
+		boolean successfulPayment = this.cardPayment.tapToPay(this.totalBalance, true);
 		assertTrue(expected == successfulPayment);
 	}
 	
 
 	@Test
-	public void testTapToPayTapEnabled() {
+	public void testNoChipInsertAttempt() {
+		String type = "Credit Card";
+		String number = "24689";
+		String cardholder = "Bob";
+		String cvv = "321";
+		String pin = "1234";
+		boolean isTapEnabled = true;
+		boolean hasChip = false; 
+		Calendar expiry = Calendar.getInstance();
+		expiry.set(Calendar.YEAR, 2023);
+		BigDecimal cardLimit = new BigDecimal(1000);
 		
+		try {
+			this.cardPayment.detectCard(type, number, cardholder, cvv, pin, isTapEnabled, hasChip, expiry, cardLimit);
+		}catch(Exception e) {
+			e.printStackTrace();
+			fail("Exception not expected"); 
+		}
+		
+		try {
+			boolean successfulPayment = this.cardPayment.tapToPay(this.totalBalance, true);
+		}catch(Exception e) {
+			assertTrue("Invalid card insertion.\n", e instanceof ChipFailureException);
+		}
+	}
+	
+	@Test
+	public void testExceedCardLimit() {
+		String type = "Credit Card";
+		String number = "24689";
+		String cardholder = "Bob";
+		String cvv = "321";
+		String pin = "1234";
+		boolean isTapEnabled = true;
+		boolean hasChip = true; 
+		Calendar expiry = Calendar.getInstance();
+		expiry.set(Calendar.YEAR, 2023);
+		//Suppose limit is less than total balance for payment 
+		BigDecimal cardLimit = new BigDecimal(50);
+		
+		try {
+			this.cardPayment.detectCard(type, number, cardholder, cvv, pin, isTapEnabled, hasChip, expiry, cardLimit);
+		}catch(Exception e) {
+			e.printStackTrace();
+			fail("Exception not expected"); 
+		}
+		
+		try {
+			boolean successfulPayment = this.cardPayment.tapToPay(this.totalBalance, true);
+		}catch(Exception e) {
+			assertTrue("Card limit is less than total balance payment.\n", e instanceof BlockedCardException);
+		}
+	}
+	
+	@Test
+	public void testValidPaymentWithChip() {
+		String type = "Credit Card";
+		String number = "24689";
+		String cardholder = "Bob";
+		String cvv = "321";
+		String pin = "1234";
+		boolean isTapEnabled = true;
+		boolean hasChip = true; 
+		Calendar expiry = Calendar.getInstance();
+		expiry.set(Calendar.YEAR, 2023);
+		BigDecimal cardLimit = new BigDecimal(1000);
+		
+		try {
+			this.cardPayment.detectCard(type, number, cardholder, cvv, pin, isTapEnabled, hasChip, expiry, cardLimit);
+		}catch(Exception e) {
+			e.printStackTrace();
+			fail("Exception not expected"); 
+		}
+		
+		try {
+			boolean successfulPayment = this.cardPayment.tapToPay(this.totalBalance, true);
+		}catch(Exception e) {
+			e.printStackTrace();
+			fail("Exception not expected"); 
+		}
+	}
+	
+	@Test
+	public void testValidPaymentWithoutChip() {
+		String type = "Credit Card";
+		String number = "24689";
+		String cardholder = "Bob";
+		String cvv = "321";
+		String pin = "1234";
+		boolean isTapEnabled = true;
+		boolean hasChip = false; 
+		Calendar expiry = Calendar.getInstance();
+		expiry.set(Calendar.YEAR, 2023);
+		BigDecimal cardLimit = new BigDecimal(1000);
+		
+		try {
+			this.cardPayment.detectCard(type, number, cardholder, cvv, pin, isTapEnabled, hasChip, expiry, cardLimit);
+		}catch(Exception e) {
+			e.printStackTrace();
+			fail("Exception not expected"); 
+		}
+		
+		try {
+			//don't insert card because hasChip==false
+			boolean successfulPayment = this.cardPayment.tapToPay(this.totalBalance, false);
+		}catch(Exception e) {
+			e.printStackTrace();
+			fail("Exception not expected"); 
+		}
 	}
 
 }
