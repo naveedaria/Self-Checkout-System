c0419617c2983482db68d98ceb6deccb451d19fb
naveedaria <naveed.aria@ucalgary.ca>
2021-03-31 19:07:47 -0600

    Final Commit

--- SelfCheckoutSystem - Software - Test/src/controlSoftware/AllTests.java
+++ SelfCheckoutSystem - Software - Test/src/controlSoftware/AllTests.java
@@ -7,10 +7,10 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({addToScalePromptTest.class, CustomerBagTest.class, BaggingAreaTest.class, DispenseChangeTest.class,
 	FinishedAddingItemsTest.class, ScanProductTest.class, RemoveItemFromScaleTest.class, ScanMemberShipCardTest.class,
-	ShoppingCartTest.class, SwipeToPayTest.class, TapToPayTest.class, WeighItemTest.class, CoinMethodTest.class, BankNoteMethodTest.class})
+	ShoppingCartTest.class, SwipeToPayTest.class, TapToPayTest.class, WeighItemTest.class, CoinMethodTest.class})
 public class AllTests {
 	
-	public static String BASEDIR = "/Users/naveed/git/SENG300Project":
+	public static String BASEDIR = "/Users/naveed/git/SENG300Project";
 
 
 }
