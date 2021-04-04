46c7d82f22a425413defcd0a76646b5dd0b107b4
jessicaljx1 <jiexin.liu1@cs.ucalgary.ca>
2021-04-04 16:44:38 -0600

    Started working on Attendant

--- SelfCheckoutSystem - Software/src/controlSoftware/Attendant.java
+++ SelfCheckoutSystem - Software/src/controlSoftware/Attendant.java
@@ -1,5 +1,18 @@
 package controlSoftware;
 
+import org.lsmr.selfcheckout.Item;
+
 public class Attendant {
+	
+	String attendantID;
+	
+	public Attendant(String attendantID) {
+		this.attendantID = attendantID;
+	}
+	
+	
+	
+	public void removeItem(ControlSoftware cs, Item item, int quantity) {
+	}
 
 }
