0239930637423be02d8316598af0a92f46de9e1a
PS1718 <sahotap1@hotmail.com>
2021-04-04 21:54:29 -0600

    working on payment gift card class

--- SelfCheckoutSystem - Software/src/controlSoftware/PaymentByGiftcard.java
+++ SelfCheckoutSystem - Software/src/controlSoftware/PaymentByGiftcard.java
@@ -1,11 +1,25 @@
 package controlSoftware;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Calendar;
+
+import org.lsmr.selfcheckout.BlockedCardException;
+import org.lsmr.selfcheckout.Card;
+import org.lsmr.selfcheckout.ChipFailureException;
+import org.lsmr.selfcheckout.InvalidPINException;
+import org.lsmr.selfcheckout.Card.CardData;
+import org.lsmr.selfcheckout.devices.CardReader;
+import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
+import org.lsmr.selfcheckout.devices.SimulationException;
+import org.lsmr.selfcheckout.external.CardIssuer;
 
 public class PaymentByGiftcard {
 	BigDecimal balance; 
 	boolean activeStatus = true;
+	private CardReader cardReader;
+	private Card inputCard;
+	//private CardIssuer cardIssuer;
 	//status field - null or active 
 	//suppose no expiry 
 	
@@ -18,30 +32,47 @@ public class PaymentByGiftcard {
 	//create separate methods based on payment by card methods (parallel) - PICKED 
 	
 	
-	public PaymentByGiftcard() {
-		//selfcheckout station parameter
+	public PaymentByGiftcard(SelfCheckoutStation selfCheckout) {
+		CardReaderListenerStub cardReaderListener = new CardReaderListenerStub();
+		selfCheckout.cardReader.register(cardReaderListener);
+		this.cardReader = selfCheckout.cardReader;
+		//this.cardIssuer = new CardIssuer("Calgary Co-op");
 		//card company is COOP 
-		//register card reader 
-		//for card itself, need cardnumber, amount
-		
-		
 		
 	}
 	
 	//other parameters will be null - pass in cardnumber, amount 
-	public void detectCard(String type, String number) {
-		
+	public void detectCard(String number) {
+		try {
+			this.inputCard = new Card("Gift Card", number, "N/A", null, null, true, false);
+			//this.cardIssuer.addCardData(number, "N/A", expiry, cvv, cardLimit);
+		}catch(SimulationException e) {
+			throw e;
+		}
 	}
 	 
-	public boolean tapToRedeem(BigDecimal amount) {
+	//value is gift card balance 
+	public boolean tapToRedeem(BigDecimal value, BigDecimal paymentBalance) throws IOException {
 		boolean cardSuccessfullyUsed=true; 
+		try {
+			CardData data= this.cardReader.tap(this.inputCard);
+			if (data==null) {
+				cardSuccessfullyUsed = false; 
+				System.out.println("Tap is not enabled.\n");
+				return cardSuccessfullyUsed;
+			}
+			
+			
+			return cardSuccessfullyUsed;
+		}catch(BlockedCardException e) {
+			throw e; 
+		}catch(SimulationException e) {
+			throw e;
+		}
 		
 		//Use Cases: card balance is greater than amount (left over amount on card)
 		//Use Case: card balance is less than amount (set status to null once amount used) 
 		//Use Case: is card is null - return a false boolean to say card wasn't used 
-		
-		
-		return cardSuccessfullyUsed;
 		
 	}
 	
