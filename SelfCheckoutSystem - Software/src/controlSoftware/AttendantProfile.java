a026a29ba696c26b22d7450525bc0bdf8315b659
Haotian Chen <47372380+haot1anchen@users.noreply.github.com>
2021-04-09 03:15:32 +0800

    Create AttendantProfile.java
    adding Samir's attendant code

---
+++ SelfCheckoutSystem - Software/src/controlSoftware/AttendantProfile.java
@@ -0,0 +1,22 @@
+package controlSoftware;
+
+import org.lsmr.selfcheckout.devices.SimulationException;
+
+public final class AttendantProfile {
+
+	private final String username;
+	private final String password;
+	
+	public AttendantProfile(String username, String password) {
+		
+		if(username == null) 
+			throw new SimulationException(new NullPointerException("username is null"));
+		
+		if(password == null) 
+			throw new SimulationException(new NullPointerException("password is null"));
+		
+		this.username = username;
+		this.password = password;
+		
+	}
+}
