d4aae4ff3f2ffe16cc00a2ee04105e686615c924
deloittecanada123 <ajaristo@ucalgary.ca>
2021-04-13 05:31:24 -0600

    Finished testing and code coverage for ScanProductUsingPLUCode method in control software

--- SelfCheckoutSystem - Software - Test/src/controlSoftware/ScanProductUsingPLUCodeTest.java
+++ SelfCheckoutSystem - Software - Test/src/controlSoftware/ScanProductUsingPLUCodeTest.java
@@ -90,5 +90,26 @@ public class ScanProductUsingPLUCodeTest {
         assertEquals( new BigDecimal(0.03).setScale(2, RoundingMode.HALF_UP),controlSoft.shoppingCart.getTotalPayment());
 		
 	}
+	
+	
+	
+	@Test
+	public void testPLUforMultipleItems() {
+		
+        PriceLookupCode plu1 = new PriceLookupCode("01864");
+        PLUCodedProduct pluProd1 = new PLUCodedProduct(plu1, "Reese's Pieces", new BigDecimal(0.25));
+        
+        Map<PriceLookupCode, PLUCodedProduct> db = ProductDatabases.PLU_PRODUCT_DATABASE;
+        db.put(plu1, pluProd1);
+        
+        // Buying 100 g of Reese's Pieces @ $0.25 / kg
+        PLUCodedItem pluCodedItem1 = new PLUCodedItem(plu1, 2000);
+        int customerItem2quantity = 3;
+        
+        controlSoft.scanProductUsingPLUCode(pluCodedItem1, customerItem2quantity);
+        
+        assertEquals( new BigDecimal(1.50).setScale(2, RoundingMode.HALF_UP),controlSoft.shoppingCart.getTotalPayment());
+		
+	}
 
 }
